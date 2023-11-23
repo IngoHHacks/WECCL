559ee728f8682efb4620c8d7f8053f21c390e097
Ingo Heijnens <ingohhacks@gmail.com>
2023-11-23 15:52:23 +0100

    Update versions

--- Plugin.cs
+++ Plugin.cs
@@ -14,7 +14,7 @@ public class Plugin : BaseUnityPlugin
 {
     public const string PluginGuid = "IngoH.WrestlingEmpire.WECCL";
     public const string PluginName = "Wrestling Empire Custom Content Loader";
-    public const string PluginVer = "1.6.0";
+    public const string PluginVer = "1.6.1";
     public const float PluginCharacterVersion = 1.56f;
     public const float PluginVersion = 1.60f;
 
