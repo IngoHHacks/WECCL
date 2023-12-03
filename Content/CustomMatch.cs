e1e84ff5f3273eb9cce966151fa82bdc928c8ad0
Tautvis <tassat12321@gmail.com>
2023-12-03 18:37:11 +0200

    Experimental custom match support
    Custom match mods will have to register their features (preset, cage, etc.) through WECCL so they get their IDs in a non-conflicting way (so different match type mods could have their own preset ids assigned by WECCL without hardcoding them in). Currently presets & cages are supported. Registering is done through WECCL.Content.RegisterCustomPreset(string name, bool positiveValue) or RegisterCustomCage(string name, bool positiveValue). positiveValue bool adds them to the positive or negative ends. TODO: optimize and add other stuff

---
+++ Content/CustomMatch.cs
@@ -0,0 +1,62 @@
+﻿namespace WECCL.Content;
+public class CustomMatch
+{
+    internal static Dictionary<string, int> CustomPresetsPos = new();
+    internal static Dictionary<string, int> CustomPresetsNeg = new();
+    internal static Dictionary<string, int> CustomCagesPos = new();
+    internal static Dictionary<string, int> CustomCagesNeg = new();
+    public static int? RegisterCustomPreset(string Name, bool PositiveValue)
+    {
+        int value;
+        if (PositiveValue)
+        {
+            if (Register(Name, CustomPresetsPos, "Preset", out value))
+            {
+                Plugin.Log.LogInfo("REGISTERED " + Name + " " + -value);
+                CustomPresetsPos.Add(Name, value);
+            }
+            return value;
+        }
+        else
+        {
+            if (Register(Name, CustomPresetsNeg, "Preset", out value))
+            {
+                Plugin.Log.LogInfo("REGISTERED " + Name + " " + -value);
+                CustomPresetsPos.Add(Name, -value);
+            }
+            return value;
+        }
+    }
+    private static bool Register(string Name, Dictionary<string, int> dictionary, string type, out int pos)
+    {
+        if (dictionary.TryGetValue(Name, out pos))
+        {
+            Plugin.Log.LogWarning(Name + " is already registered as " + type + " " + pos);
+            return false;
+        }
+        pos = 10001 + dictionary.Count;
+        return true;
+    }
+    public static int? RegisterCustomCage(string Name, bool PositiveValue)
+    {
+        int value;
+        if (PositiveValue)
+        {
+            if (Register(Name, CustomCagesPos, "Cage", out value))
+            {
+                Plugin.Log.LogInfo("REGISTERED " + Name + " " + value);
+                CustomPresetsPos.Add(Name, value);
+            }
+            return value;
+        }
+        else
+        {
+            if (Register(Name, CustomCagesNeg, "Cage", out value))
+            {
+                Plugin.Log.LogInfo("REGISTERED " + Name + " " + -value);
+                CustomPresetsPos.Add(Name, -value);
+            }
+            return value;
+        }
+    }
+}
---
+++ Patches/MatchTypePatch.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine.SceneManagement;
+using WECCL.Content;
+namespace WECCL.Patches;
+
+[HarmonyPatch]
+public class MatchTypePatch
+{
+    private static float OldPresetMax;
+    private static float OldPresetMin;
+    private static float OldCageMax;
+    private static float OldCageMin;
+   
+    [HarmonyPatch(typeof(UnmappedMenu), nameof(UnmappedMenu.ODONMLDCHHF))]
+    [HarmonyPrefix]
+    private static void CustomPresetPrefix(UnmappedMenu __instance, float __result, ref float CAAJBNHEFJJ, float OEGLNPMNEOE, float NOGFHHECJBM, ref float JBPAELFIDOP, ref float LKBOHHGFJFO, int LKJHMOHMKCM)
+    {
+        if (SceneManager.GetActiveScene().name != "Match_Setup") return;
+        if (LIPNHOMGGHF.CHLJMEPFJOK == 2)
+        {
+            if (LIPNHOMGGHF.FKANHDIMMBJ[1] == __instance)
+            {
+                OldPresetMax = LKBOHHGFJFO;
+                OldPresetMin = JBPAELFIDOP;
+                JBPAELFIDOP -= CustomMatch.CustomPresetsNeg.Count;
+                LKBOHHGFJFO += CustomMatch.CustomPresetsPos.Count;
+                if (CAAJBNHEFJJ >= 10000)
+                {
+                    CAAJBNHEFJJ = CAAJBNHEFJJ - 10000 + OldPresetMax;
+                }
+                if (CAAJBNHEFJJ <= -10000)
+                {
+                    CAAJBNHEFJJ = CAAJBNHEFJJ + 10000 - OldPresetMin;
+                }
+            }
+        }    
+    }
+    [HarmonyPatch(typeof(UnmappedMenu), nameof(UnmappedMenu.ODONMLDCHHF))]
+    [HarmonyPostfix]
+    private static void CustomPresetPostfix(UnmappedMenu __instance, ref float __result, float CAAJBNHEFJJ, float OEGLNPMNEOE, float NOGFHHECJBM, ref float JBPAELFIDOP, ref float LKBOHHGFJFO, int LKJHMOHMKCM)
+    {
+        if (SceneManager.GetActiveScene().name != "Match_Setup") return;
+        if (LIPNHOMGGHF.CHLJMEPFJOK == 2)
+        {
+            if (LIPNHOMGGHF.FKANHDIMMBJ[1] == __instance)
+            {
+                if (__result > OldPresetMax)
+                {
+                    __result = 10000 + (__result - OldPresetMax);
+                }
+                if (__result < OldPresetMin) __result = -10000 - (OldPresetMin - __result);
+            }
+        }
+    }
+    [HarmonyPatch(typeof(UnmappedMenu), nameof(UnmappedMenu.ODONMLDCHHF))]
+    [HarmonyPrefix]
+    private static void CustomCagePrefix(UnmappedMenu __instance, float __result, ref float CAAJBNHEFJJ, float OEGLNPMNEOE, float NOGFHHECJBM, ref float JBPAELFIDOP, ref float LKBOHHGFJFO, int LKJHMOHMKCM)
+    {
+        if (SceneManager.GetActiveScene().name != "Match_Setup") return;
+        if (LIPNHOMGGHF.CHLJMEPFJOK == 1 && LIPNHOMGGHF.ODOAPLMOJPD == 1)
+        {
+            if (LIPNHOMGGHF.FKANHDIMMBJ[9] == __instance)
+            {
+                OldCageMax = LKBOHHGFJFO;
+                OldCageMin = JBPAELFIDOP;
+                JBPAELFIDOP -= CustomMatch.CustomCagesNeg.Count;
+                LKBOHHGFJFO += CustomMatch.CustomCagesPos.Count;
+                if (CAAJBNHEFJJ >= 10000)
+                {
+                    CAAJBNHEFJJ = CAAJBNHEFJJ - 10000 + OldCageMax;
+                }
+                if (CAAJBNHEFJJ <= -10000)
+                {
+                    CAAJBNHEFJJ = CAAJBNHEFJJ + 10000 - OldCageMin;
+                }
+            }
+        }
+    }
+    [HarmonyPatch(typeof(UnmappedMenu), nameof(UnmappedMenu.ODONMLDCHHF))]
+    [HarmonyPostfix]
+    private static void CustomCagePostfix(UnmappedMenu __instance, ref float __result, float CAAJBNHEFJJ, float OEGLNPMNEOE, float NOGFHHECJBM, ref float JBPAELFIDOP, ref float LKBOHHGFJFO, int LKJHMOHMKCM)
+    {
+        if (SceneManager.GetActiveScene().name != "Match_Setup") return;
+        if (LIPNHOMGGHF.CHLJMEPFJOK == 1 && LIPNHOMGGHF.ODOAPLMOJPD == 1)
+        {
+            if (LIPNHOMGGHF.FKANHDIMMBJ[9] == __instance)
+            {
+                if (__result > OldCageMax)
+                {
+                    __result = 10000 + (__result - OldCageMax);
+                }
+                if (__result < OldCageMin) __result = -10000 - (OldCageMin - __result);
+            }
+        }
+    }
+}
